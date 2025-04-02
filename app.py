6152107e71bc61bada6609a36c96e006c116c993
SivaRamaRaju2310 <pakalapatisivaramaraju@gmail.com>
2025-04-02 12:06:13 +0530

    commited

--- app.py
+++ app.py
@@ -106,5 +106,8 @@ def detect():
 
     return response
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))  # Default to 10000
+    app.run(host="0.0.0.0", port=port, debug=True)
+
